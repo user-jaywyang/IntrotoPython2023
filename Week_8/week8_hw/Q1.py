535cfc19e22b916e60906bb66242db3f2769feea
user-jaywyang <156152290+user-jaywyang@users.noreply.github.com>
2025-01-21 04:37:59 -0800

    Create Q1.py

---
+++ Week_8/week8_hw/Q1.py
@@ -0,0 +1,14 @@
+import Q1class
+
+def main():
+    #construct three instances of class Information
+    subject1 = Q1class.Information('Jay', '111 1nd Ave', 26, '111-111-1111')
+    subject2 = Q1class.Information('Sally', '222 2nd Ave', 24, '222-222-2222')
+    subject3 = Q1class.Information('Patrick', '333 3nd Ave', 25, '333-333-3333')
+
+    print(subject1)
+    print(subject2)
+    print(subject3)
+
+if __name__ == '__main__':
+    main()
