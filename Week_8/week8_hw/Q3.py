f5ae758d2ef4fa6514991954a8a1789e17ea0be3
user-jaywyang <156152290+user-jaywyang@users.noreply.github.com>
2025-01-21 04:39:24 -0800

    Create Q3.py

---
+++ Week_8/week8_hw/Q3.py
@@ -0,0 +1,15 @@
+import Q3class
+
+def main():
+    #construct three instances of class Employee
+    item1 = Q3class.RetailItem('Jacket', 12, 59.95)
+    item2 = Q3class.RetailItem('Designer Jeans', 40, 34.95)
+    item3 = Q3class.RetailItem('Shirt', 20, 24.95)
+
+    print(item1)
+    print(item2)
+    print(item3)
+
+
+if __name__ == '__main__':
+    main()
