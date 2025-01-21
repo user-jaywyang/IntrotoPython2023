cb1f1bf7438a7123bb4db4f7ab2f36388883b806
user-jaywyang <156152290+user-jaywyang@users.noreply.github.com>
2025-01-21 04:45:54 -0800

    Create Q1.py

---
+++ Week_9/week9_hw/Q1.py
@@ -0,0 +1,10 @@
+import Q1classes
+
+def main():
+    #creates an instance of Customer which is a subclass of Person
+    jay = Q1classes.Customer('Jay', '222 Ave', '222-222-2222', 222222, True)
+
+    print(jay)
+
+if __name__ == '__main__':
+    main()
